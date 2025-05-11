aab2f5740684d504e53b43cbb9cd115eb2c3680d
Danila <danil.lysunckin@gmail.com>
2025-05-11 21:03:41 +0500

    code review

--- src/main/resources/db/migration/V1_2__initial_users.sql
+++ src/main/resources/db/migration/V1_2__initial_users.sql
@@ -1,3 +1,2 @@
 INSERT INTO users(username, password, role)
-VALUES ('user', '$2a$12$bWxmaKn9q8ZiNrAy4PBnUOXv47jX2remiq0M1qCbxmzXBh1tHyYI6', 'ROLE_USER'),
-       ('admin', '$2a$12$bWxmaKn9q8ZiNrAy4PBnUOXv47jX2remiq0M1qCbxmzXBh1tHyYI6', 'ROLE_ADMIN');
+VALUES ('admin', '$2a$12$bWxmaKn9q8ZiNrAy4PBnUOXv47jX2remiq0M1qCbxmzXBh1tHyYI6', 'ROLE_ADMIN');
